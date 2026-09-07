f6992cfda5d8849dd2360264f75fde027a12af47
iamanrawal <amanrawal1809@gmail.com>
2026-09-07 23:08:41 +0530

    .

---
+++ Practice/C/2026-09-07-floataddition.c
@@ -0,0 +1,17 @@
+ #include <stdio.h>
+int main()
+{
+
+  float x,y,sum ;
+
+printf("enter any two numbers");
+
+scanf("%f %f", &x, &y );
+
+sum = x + y;
+
+printf("Sum = %f", sum);
+
+return 0;
+
+}
